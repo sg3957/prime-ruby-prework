f9c34fd69ee15c2f6762e39bddcb654ae367edf0
Sydney Grant <sg3957@nyu.edu>
2018-09-26 00:54:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+  if num == 2 
+    return true 
+  elsif num % 2 == 0 
+    return false
+  elsif num <=1
+    return false
+  else
+    return true
+  end
+end
